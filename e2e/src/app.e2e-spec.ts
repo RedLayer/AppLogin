dc01480ac80663901c2074f3cbcc2b5109f8a953
Pablo Fernandez <pablof.a3f.4@gmail.com>
2020-01-21 15:56:51 +0100

    Commit de ejercicio de aplicacion de login

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to AppLogin!');
+    expect(page.getTitleText()).toEqual('Welcome to loginApp!');
   });
 
   afterEach(async () => {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: 'YOUR_FIRE_BASE_CONFIG',
+  tiempExpirar: 30
 };
 
 /*
